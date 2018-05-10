f6d64e415aa821709ea86057f08fcf4b252ad0f5
vytautas <vytautas.rimeikis@gmail.com>
2018-05-11 00:39:12 +0300

    day 5 array for data

---
+++ 5_lesson/data.php
@@ -0,0 +1,62 @@
+<?php
+
+$masyvas1 = [0, 4, 1, 2, 2, 65, 3, 3, 4, 2];
+
+$masyvas2 = ['Jonas', 'Petras', 'Antanas',];
+
+$masyvas3 = [
+    0 => [
+        0 => '1 kabinetas',
+        1 => '2 kabinetas',
+        2 => '3 kabinetas',
+    ],
+    1 => [
+        0 => '4 kabinetas',
+        1 => '5 kabinetas',
+        2 => '6 kabinetas',
+    ],
+];
+
+$masyvas4 = [
+    [
+        'vardas' => 'Petras',
+        'pavarde' => 'Petraitis',
+        'amzius' => 23,
+    ],
+    [
+        'vardas' => 'Jonas',
+        'pavarde' => 'Jonaitis',
+        'amzius' => 24,
+    ],
+    [
+        'vardas' => 'Tomas',
+        'pavarde' => 'Pavardenis',
+        'amzius' => 33,
+    ],
+    [
+        'vardas' => 'Vardas',
+        'pavarde' => 'Jonusas',
+        'amzius' => 28,
+    ],
+];
+
+$masyvas5 = [
+    [
+        'marke' => 'Audi',
+        'model' => 'A6',
+        'kubatura' => 1995,
+        'metai' => 2016,
+    ],
+    [
+        'marke' => 'BMW',
+        'model' => 'M3',
+        'kubatura' => 2995,
+        'metai' => 2018,
+    ],
+    [
+        'marke' => 'BMW',
+        'model' => '320',
+        'kubatura' => 1999,
+        'metai' => 2017
+    ]
+];
