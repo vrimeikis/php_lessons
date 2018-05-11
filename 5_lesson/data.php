f67059494b55fc3b5e6d5839122b1ca5ec065078
vytautas <vytautas.rimeikis@gmail.com>
2018-05-11 12:28:04 +0300

    add array to data file

--- 5_lesson/data.php
+++ 5_lesson/data.php
@@ -60,3 +60,22 @@ $masyvas5 = [
         'metai' => 2017
     ]
 ];
+
+$masyvas6 = [
+    [
+        'bank' => 'swed',
+        'payment_title' => 'Mokejimas uz prekes 1',
+        'total' => 21.32,
+    ],
+    [
+        'bank' => 'seb',
+        'payment_title' => 'Mokejimas uz paslaugas',
+        'total' => 32.52,
+    ],
+    [
+        'bank' => 'dnb',
+        'payment_title' => 'Mokejimas uz prekes 2',
+        'total' => 12.84,
+    ],
+
+];
