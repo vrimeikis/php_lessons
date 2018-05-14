d08c8777a5d716b7f30a314367d23a34752118f6
vytautas <vytautas.rimeikis@gmail.com>
2018-05-14 12:58:20 +0300

    more data

--- 6_lesson/data.php
+++ 6_lesson/data.php
@@ -25,4 +25,29 @@ $data = (object)[
     ],
 ];
 
+$data1 = (object)[
+    'cart' => (object)[
+        'products' => [
+            (object)[
+                'id' => 23,
+                'title' => 'Duona',
+                'price' => 1.23,
+                'quantity' => 2,
+            ],
+            (object)[
+                'id' => 43,
+                'title' => 'Suris',
+                'price' => 3.65,
+                'quantity' => 1,
+            ],
+        ],
+    ],
+    'customer' => (object)[
+        'name' => 'Petras',
+        'lastname' => 'Petraitis',
+        'email' => 'petras@petraitis.com',
+    ],
+];
+
+
 $jsonData = '{"cart":{"products":[{"id":23,"title":"Duona","price":1.23,"quantity":1},{"id":43,"title":"Suris","price":3.65,"quantity":1}],"total":4.88},"customer":{"name":"Petras","lastname":"Petraitis","email":"petras@petraitis.com"}}';
